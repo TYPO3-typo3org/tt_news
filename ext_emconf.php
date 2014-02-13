1edcd878d69ed5ac35aec9f688ced3e8551e3e15
christian.zenker <christian.zenker@e8cb78a8-64f2-4011-8042-bd1ea5058d47>
2014-02-13 14:26:05 +0000

    add official release of tt_news 3.5.2

---
+++ ext_emconf.php
@@ -0,0 +1,39 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "tt_news".
+ *
+ * Auto generated 13-02-2014 09:51
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array (
+	'title' => 'News',
+	'description' => 'Website news with front page teasers and article handling inside.',
+	'category' => 'plugin',
+	'shy' => 0,
+	'version' => '3.5.2',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'TYPO3_version' => '4.5.0-6.1.99',
+	'PHP_version' => '5.2.0-5.4.99',
+	'module' => 'mod1',
+	'state' => 'beta',
+	'uploadfolder' => 1,
+	'createDirs' => '',
+	'modify_tables' => 'be_groups,be_users',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 'Rupert Germann [wmdb]',
+	'author_email' => 'rupi@gmx.li',
+	'author_company' => 'www.wmdb.de',
+	'CGLcompliance' => NULL,
+	'CGLcompliance_note' => NULL,
+);
+
+?>
